93c8c64fb293bcf35761fff05cb0bf7b948c95cf
Khanh Nguyen <kingstyle1995@gmail.com>
2023-03-11 17:26:59 +0700

    Video-08-Fetch Products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import data from '../data';
+// import data from '../backend/data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
